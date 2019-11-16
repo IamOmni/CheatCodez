77dba7be78a82600ee299c7fcb28a77f952df70e
georgetles <glesbirel@gmail.com>
2019-11-16 14:44:53 +0000

    Test Commit + Base config changes

---
+++ core/src/com/kroy/modules/DB.java
@@ -0,0 +1,18 @@
+package com.kroy.modules;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DB {
+    public Map<String, Integer> getHighScores() {
+        Map<String, Integer> scores = new HashMap<>();
+
+        return scores;
+    };
+
+    public void addScore(){
+        return;
+    }
+
+
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.kroy.game.mainKroyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1080;
+		config.height = 720;
+		config.title = "Kroy - TeamCheatCodez";
 		new LwjglApplication(new mainKroyGame(), config);
 	}
 }
