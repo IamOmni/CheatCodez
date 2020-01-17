7e7bcd876a1bb798891bc169248b342e9490ab88
SwirlyClouds <muazatif@protonmail.com>
2020-01-17 01:14:29 +0000

    PPM and optimise import

--- core/src/com/kroy/screens/PlayScreen.java
+++ core/src/com/kroy/screens/PlayScreen.java
@@ -1,35 +1,13 @@
 package com.kroy.screens;
 
 import com.badlogic.gdx.Screen;
-
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.kroy.classes.*;
-
-import com.kroy.classes.Object;
-import com.kroy.pathfinding.Agent;
-import com.kroy.pathfinding.Coord;
-import com.kroy.pathfinding.MapGraph;
 import com.kroy.game.kroyGame;
 
 public class PlayScreen implements Screen {
