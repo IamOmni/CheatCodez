6f0a94cc5893680eff61d5aed4f800150bae1471
SwirlyClouds <muazatif@protonmail.com>
2020-01-17 00:46:04 +0000

    implement methods

--- core/src/com/kroy/screens/PlayScreen.java
+++ core/src/com/kroy/screens/PlayScreen.java
@@ -40,10 +40,45 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class PlayScreen implements Screen, InputProcessor {
+public class PlayScreen implements Screen {
     private kroyGame game;
 
     public PlayScreen(kroyGame game){
         this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
     }
 }
